6669f749aeb8b76a290ebc05f8f58e208faea10f
Theo Armour <t.armour@gmail.com>
2025-05-17 17:39:02 -0700

    Add initial implementation of GitHub repository viewer and styling
    - Created `github-tree-view.js` to fetch and display repository contents using GitHub API.
    - Developed `style-core.css` and `style.css` for consistent styling across the application.
    - Implemented `tootoo.html` as the main interface for the application, integrating navigation and content display.
    - Added `index-2025-05-11.html` as a landing page with relevant metadata and links.
    - Included responsive design features for better usability on various screen sizes.
    - Established a structure for managing user interactions and content rendering.

--- code/2025-05-17/main.js
+++ code/2025-05-17/main.js
@@ -15,10 +15,15 @@ const COR = {
     urlPushPath: "https://heritage-happenings.github.io/",
 
     menuTitle: "Heritage Happenings",
-    version: "Version: 2025-05-17 14:29",
+    version: "Version: 2025-05-17 17:36",
  
     filterFolders: ["activities", "code", "files", "history", "kiosk", "people", "residents-council"], //[ "code"],
 
     ignoreFiles: ["404.html", "favicon.ico", "index.html", "readme.html"],
 }
+
+/* 0 to 360 10=red 120=green 240=blue */
+let r = document.querySelector(':root');
+r.style.setProperty('--main-hue', '10');
+r.style.setProperty('--mnu-width', '17rem');
 
