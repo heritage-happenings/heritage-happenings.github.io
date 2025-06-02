1b0f6480375d640733c6d814ad2c50d1cf6ef749
Theo Armour <t.armour@gmail.com>
2025-06-01 17:38:22 -0700

    Add initial implementation of TooToo 2025 with GitHub API integration
    - Created main JavaScript file (main.js) for core functionality including text scaling and dynamic styling.
    - Developed HTML structure for the application (tootoo.html, tootoo-edit.html, readme.html) with responsive design and navigation.
    - Implemented CSS styles (style-core.css) for consistent theming and layout across the application.
    - Configured application settings in config.js, including repository details and file filters.
    - Added functionality for text size adjustment and persistent storage of user preferences.
    - Integrated Showdown library for Markdown rendering.

---
+++ tootoo/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+
+    user: "heritage-happenings",
+    repo: "heritage-happenings.github.io",
+    branch: "master",
+
+    menuTitle: "Heritage Happenings",
+    menuTitleEdit: "HH Edit",
+
+    defaultFile: ".md",
+    defaultFileEdit: "@@README.md",
+
+    filterFolders: ["activities", "files", "history", "kiosk", "people", "residents-council", "tootoo"],
+    ignoreFiles: ["404.html", "favicon.ico", "index.html", "readme.html", "test.md"],
+
+}
