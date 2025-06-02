a9e739432375131ce548446fa54f399a6502a5b5
Theo Armour <t.armour@gmail.com>
2025-06-01 22:07:30 -0700

    Update HTML files to include config.js and main.js scripts, fix date in metadata, and adjust URL paths for navigation

--- tootoo/2025-06-01/main.js
+++ tootoo/2025-06-01/main.js
@@ -1,4 +1,6 @@
-const folder = CONFIG.repo.includes( "github.io") ? "" : CONFIG.repo;
+      const folder = CONFIG.repo.includes( "github.io") ? "" : CONFIG.repo;
+
+
 
 const COR = {
   
@@ -19,10 +21,9 @@ const COR = {
   filterFolders: CONFIG.filterFolders, //[], //[ "tootoo"],
   ignoreFiles: CONFIG.ignoreFiles, //[], //[ "404.html", "favicon.ico", "index.html", "LICENSE", "readme.html" ],
 
-
   urlBaseAPI: `https://api.github.com/repos/${CONFIG.user}/${CONFIG.repo}/contents/`,
   urlSource: `https://github.com/${CONFIG.user}/${CONFIG.repo}/tree/main/`,
-  urlPathApps: `https://${CONFIG.user}.github.io/tootoo/`,
+  urlPathApps: `https://${CONFIG.user}.github.io/${CONFIG.repo}/tootoo/`,
   urlPathContent: `https://${CONFIG.user}.github.io/${folder}/`,
   urlPushPath: `https://${CONFIG.user}.github.io/${folder}/`,
 
